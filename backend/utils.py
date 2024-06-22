8bdd67c57c012633a91b0dbb25a637cd95a92aa5
hyanaka <166586642+hyanaka@users.noreply.github.com>
2024-06-23 00:12:18 +0900

    change: Remove "==" from short_uuid() output to reduce length

--- backend/utils.py
+++ backend/utils.py
@@ -4,4 +4,4 @@ from base64 import urlsafe_b64encode
 def short_uuid():
     uuid_bytes = uuid4().bytes
     uuid_base64 = urlsafe_b64encode(uuid_bytes).decode()
-    return uuid_base64
+    return uuid_base64[:-2]
