7aa7e6a4fe4c3ba4275dcecf97e4446308ab162f
hyanaka <166586642+hyanaka@users.noreply.github.com>
2024-07-02 10:19:24 +0900

    feat: Header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-
+import Header from './components/Header.vue';
 </script>
 
 <template>
-
+  <v-app>
+    <Header></Header>
+  </v-app>
 </template>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+const has_id = ref(true);
+</script>
+
+<template>
+    <v-app-bar>
+        <v-app-bar-title>
+            <router-link class="app-bar-link" style="width: 5.5em;" to="/">
+                memnote
+            </router-link>
+        </v-app-bar-title>
+        <router-link class="app-bar-link" to="/notes/new">新規ノート</router-link>
+        <template v-if="has_id">
+            <router-link class="app-bar-link" to="/mynotes">マイノート</router-link>
+        </template>
+    </v-app-bar>
+</template>
+
+<style scoped>
+.v-app-bar-title{
+    height: 100%;
+    align-self: center;
+    font-size: 2.2rem;
+}
+.app-bar-link{
+    display: flex;
+    color: black;
+    text-decoration: none;
+    font-weight: bold;
+    padding: 20px 5px 20px 5px;
+    margin: 0 10px 0 10px;
+}
+</style>
