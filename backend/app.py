90777d2c25df1c07c28229c0f06c43e9509ac292
hyanaka <166586642+hyanaka@users.noreply.github.com>
2024-07-12 22:02:13 +0900

    feat: CORS

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from api import api_bp
 import models
 
 app = Flask(__name__, static_folder='../frontend/dist/static', template_folder='../frontend/dist')
 app.config.from_pyfile('app.cfg')
+CORS(app)
 
 app.register_blueprint(api_bp)
 models.init(app)
