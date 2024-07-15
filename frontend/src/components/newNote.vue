c006363fd0db8f9bb29e83e6fd4dbd0db6a69784
hyanaka <166586642+hyanaka@users.noreply.github.com>
2024-07-15 10:57:47 +0900

    add: Note adding component

---
+++ frontend/src/components/newNote.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useUID } from '../composables/useUID';
+import { useAPI } from '../composables/useAPI';
+import { useRouter } from 'vue-router';
+
+const APIs = useAPI(await useUID());
+const nid = await APIs.addNote();
+const router = useRouter();
+router.push({ name: 'Note', params: { id: nid}});
+</script>
+
+<template>
+    <p>ノートを作成中...</p>
+</template>
+
+<style scoped>
+p{
+    text-align: center;
+    margin: 20px;
+}
+</style>
