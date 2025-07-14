8c7eff335fbc95b04f55dce137408f5adb5ba24a
k4nk1 <166586642+k4nk1@users.noreply.github.com>
2025-07-14 20:40:25 +0900

    refactor: style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,7 +6,7 @@ import Footer from './components/Footer.vue';
 <template>
   <v-app>
     <Header />
-    <v-main style="min-height: 100vh;">
+    <v-main style="min-height: calc(100vh - 64px);">
       <router-view />
     </v-main>
     <Footer />
--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <v-app-bar>
         <v-app-bar-title>
-            <router-link class="app-bar-link" style="width: 5.5em;" to="/">
-                memnote
+            <router-link class="app-bar-link" to="/">
+                <img class="app-bar-logo" src="../assets/memnote.svg">
             </router-link>
         </v-app-bar-title>
         <router-link class="app-bar-link" to="/newnote">新規ノート</router-link>
@@ -13,15 +13,24 @@
 <style scoped>
 .v-app-bar-title{
     height: 100%;
+    margin-left: 0;
     align-self: center;
     font-size: 2.2rem;
 }
+.v-toolbar-title{
+    height: 64px;
+}
 .app-bar-link{
-    display: flex;
     color: black;
     text-decoration: none;
     font-weight: bold;
-    padding: 20px 5px 20px 5px;
-    margin: 0 10px 0 10px;
+    padding: 20px 0.5vw 20px 0.5vw;
+    margin: 0 0.5vw;
+}
+.app-bar-logo{
+    max-width: calc(100vw - 180px);
+    max-height: 64px;
+    padding: 10px 1vw;
+    margin: auto 0;
 }
 </style>
